87b1711b22151ee5be06850b0ec593fe5aa7e812
Nora <nizcat5@gmail.com>
2026-01-11 13:49:42 -0800

    2026 changes

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "TEMPLATE-2026";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 27;
+  public static final String GIT_SHA = "5ada3a8b8ea972abcb2f5b662334f9b199ec4789";
+  public static final String GIT_DATE = "2025-11-20 22:32:53 EST";
+  public static final String GIT_BRANCH = "feat/improved-lib";
+  public static final String BUILD_DATE = "2025-12-01 20:04:19 EST";
+  public static final long BUILD_UNIX_TIME = 1764637459630L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
